a0bef9d4d6aae08f1b4a27ff4499fd500664fdd9
EC2 Default User <ec2-user@ip-172-31-23-76.ap-northeast-1.compute.internal>
2023-06-05 07:24:30 +0000

    検索

--- app/models/review.rb
+++ app/models/review.rb
@@ -3,4 +3,17 @@ class Review < ApplicationRecord
   belongs_to :user
   has_many :commments
 
+ def self.looks(search, word)
+    if search == "perfect_match"
+      @review = Review.where("title LIKE?","#{word}")
+    elsif search == "forward_match"
+      @review = Review.where("title LIKE?","#{word}%")
+    elsif search == "backward_match"
+      @review = Review.where("title LIKE?","%#{word}")
+    elsif search == "partial_match"
+      @review = Review.where("title LIKE?","%#{word}%")
+    else
+      @review = Review.all
+    end
+ end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -22,5 +22,7 @@ Rails.application.routes.draw do
     resources :reviews, only: [:index,:show,:destroy,]
     resources :users, only: [:index,:show,:edit,:update]
   end
+  
+  get "search" => "searches#search"
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
